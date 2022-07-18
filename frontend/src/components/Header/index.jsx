3a864ce5f2fcb843822b0ab600ddc5a4b312688b
Pedro Ribas <pedroibribas@gmail.com>
2022-07-17 21:31:30 -0400

    Commit Header and Home update

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,83 +1,46 @@
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import { FaSignInAlt, FaUser } from 'react-icons/fa';
+import { FaUser } from 'react-icons/fa';
+import { ImPencil2 } from 'react-icons/im';
 import { AuthContext } from '../../context/Context';
 import { HeaderLogout } from './HeaderLogout';
 import styles from './styles.module.scss';
 
 export function Header() {
   const { user } = useContext(AuthContext);
-  const [isActive, setIsActive] = useState({
-    isHomeActive: false,
-    isWriteActive: false,
-  });
-
-  const handleLinkClick = e => {
-    if (e.target.id === 'Home') {
-      setIsActive(prevState => ({
-        ...prevState,
-        isHomeActive: true,
-        isWriteActive: false,
-      }))
-    }
-
-    if (e.target.id === 'Write') {
-      setIsActive(prevState => ({
-        ...prevState,
-        isHomeActive: false,
-        isWriteActive: true,
-      }))
-    }
-  }
 
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <Link className={styles.headerLogo} to="/">
-          BlogIn
+          <ImPencil2 />
         </Link>
 
         <nav>
-          <Link
-            id="Home"
-            className={isActive.isHomeActive ? styles.active : ''}
-            to="/"
-            onClick={handleLinkClick}
-          >
-            Home
-          </Link>
-          <Link
-            id="Write"
-            className={isActive.isWriteActive ? styles.active : ''}
-            to="/write"
-            onClick={handleLinkClick}
-          >
-            Escrever
-          </Link>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/write">Escrever</Link></li>
+            {user ? (
+              <>
+                <li>
+                  <HeaderLogout />
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <Link to="/login">Login</Link>
+                </li>
+                <li>
+                  <Link className={styles.registerBtn} to="/register">
+                    <FaUser /> Registrar
+                  </Link>
+                </li>
+              </>
+            )}
+          </ul>
         </nav>
 
-        <ul className={styles.rightContent}>
-          {user ? (
-            <>
-              <li>
-                <HeaderLogout />
-              </li>
-            </>
-          ) : (
-            <>
-              <li>
-                <Link className={styles.loginBtn} to="/login">
-                  <FaSignInAlt /> Login
-                </Link>
-              </li>
-              <li>
-                <Link className={styles.registerBtn} to="/register">
-                  <FaUser /> Registrar
-                </Link>
-              </li>
-            </>
-          )}
-        </ul>
       </div>
     </header>
   )
--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -5,13 +5,17 @@ import styles from './styles.module.scss';
 
 export function Home() {
   const [posts, setPosts] = useState([]);
+  const year = new Date().getFullYear();
 
   useEffect(() => {
     const fetchPosts = async () => {
       try {
         const res = await axios("/api/posts");
 
-        setPosts(res.data);
+        const postsArr = res.data;
+        const posts = postsArr.reverse().slice(0, 6);
+
+        setPosts(posts);
       } catch (error) {
         console.log(error.response.data)
       }
@@ -21,15 +25,29 @@ export function Home() {
   }, []);
 
   return (
-    <div className={styles.homeContainer}>
-      <div className={styles.homeContent}>
-        {posts.map(post => (
-          <PostItem
-            key={post._id}
-            post={post}
-          />
-        ))}
-      </div>
-    </div>
+    <>
+      <section className={styles.homeContainer}>
+        <div className={styles.heroContent}>
+          <h1>Pensou? <span>Compartilhe.</span></h1>
+          <span className={styles.subtitle}>
+            Crie uma conta pessoal e comece a compartilhar ideias com o mundo.
+          </span>
+        </div>
+
+        <div className={styles.postsContent}>
+          <h2>Últimas publicações</h2>
+          <div className={styles.grid}>
+            {posts.map(post => (
+              <PostItem
+                key={post._id}
+                post={post}
+              />
+            ))}
+          </div>
+        </div>
+
+      </section>
+      <footer className={styles.footer}>BlogIn &#64;{year}</footer>
+    </>
   );
 };
